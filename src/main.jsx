9fd60efbee9fe342a51b067b0e5e3f3c8aad09c4
Tatiana Cuadros <cuadrost31@gmail.com>
2025-06-27 00:01:24 -0500

    Instalación de Redux e iniciando con autenticación de ususarios

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import { JournalApp } from './JournalApp.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { JournalApp } from './JournalApp.jsx';
+import { store } from './store/index.js';
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
